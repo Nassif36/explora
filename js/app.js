dfdc86878e628811aa3b4d21c16dbe8afa49d79d
Nassif36 <xsrkhalil@gmail.com>
2022-01-21 07:36:22 -0300

    retouch

--- js/app.js
+++ js/app.js
@@ -9,3 +9,27 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 850px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+    var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.innerHTML = "Gracias, nos pondremos en contacto con vos!";
+        form.reset()
+    }).catch(error => {
+        status.innerHTML = "Oops! no olvides completar los campos"
+    });
+}
+form.addEventListener("submit", handleSubmit) 
+  
