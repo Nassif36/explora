a8cc55a2adf90db50e44b241021d0d4feb917911
Nassif36 <xsrkhalil@gmail.com>
2022-03-21 20:02:46 -0300

    push

--- js/app.js
+++ js/app.js
@@ -6,30 +6,60 @@ function myFunction(x) {
     }
 }
 
-var x = window.matchMedia("(max-width: 850px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+const getSubmitData = () => {
+    event.preventDefault()
+    let name = document.getElementById('form-name').value;
+    let number = document.getElementById('form-number').value;
+    let to = document.getElementById('form-mail').value;
+    let subject = document.getElementById('form-asunto').value;
+    let body = document.getElementById('form-mensaje').value;
+    if (name.value <= 0) {
+        alert('completa los campos')
+    } else {
+
+    }
+    //show the loading animation
+    document.getElementById('loading').style.display = 'block';
+
+    let url = "https://email-test-khali.herokuapp.com";
+    let data = {
+        name,
+        number,
+        to,
+        subject,
+        body,
+    };
 
 
 
-    var form = document.getElementById("my-form");
+    fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.status === 'ok') {
+                document.getElementById('form-name').value = '';
+                document.getElementById('form-number').value = '';
+                document.getElementById('form-mail').value = '';
+                document.getElementById('form-asunto').value = '';
+                document.getElementById('form-mensaje').value = '';
+                document.getElementById('loading').style.display = 'none';
+                document.getElementById('my-form-status').innerHTML = 'Mensaje enviado correctamente';
+                document.getElementById('my-form-status').style.background.color = '#008000';
+                document.getElementById('my-form-status').style.border.radius = '3px';
+                document.getElementById('my-form-status').style.maxWidth = '450px';
+            } else {
 
-async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("my-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        status.innerHTML = "Gracias, nos pondremos en contacto con vos!";
-        form.reset()
-    }).catch(error => {
-        status.innerHTML = "Oops! no olvides completar los campos"
-    });
-}
-form.addEventListener("submit", handleSubmit) 
+                document.getElementById('my-form-status').style.color = '#008000';
+                document.getElementById('my-form-status').style.border.radius = '3px';
+                document.getElementById('my-form-status').style.padding = '1em 2em';
+                document.getElementById('my-form-status').innerHTML = 'Ooops.. ocurrio un error estamos trabajando en ello';
+            }
+        });
+
+};
   
